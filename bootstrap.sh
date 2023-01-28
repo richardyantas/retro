cd22aa4372faeaf5ef1516f7e335ac83e908f72e
Richard Yantas <richard@dysopsis.com>
2023-01-28 16:17:03 -0500

    plop

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
+#!/bin/bash
 sudo apt-get update
-sudo apt-get install -yq build-essential python3-pip rsync screen
-pip install pipenv
+sudo apt-get install -yq build-essential python3-pip rsync screen git pipenv
 git clone https://github.com/richardyantas/tenpo.git
 cd tenpo/
-pipenv shell
+sudo pipenv shell
 pipenv install
 screen -S app
 python3 app.py&
