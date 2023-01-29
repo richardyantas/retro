c97f7d068034e341ae9d71e1fca48e0ed2cec1a2
Richard Yantas <richard@dysopsis.com>
2023-01-28 21:38:21 -0500

    fresh

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
 from flask import Flask, request, jsonify
-import json
-import pickle
-import pandas as pd
-import numpy as np
+from inference import predict
+
+# curl -XPOST -d @./data.json -H "Content-Type:application/json" http://127.0.0.1:5000
 
 app = Flask(__name__)
 
-# Load the model
-model = pickle.load(open('model.pkl','rb'))
-labels ={
-  0: "versicolor",   
-  1: "setosa",
-  2: "virginica"
-}
-
-@app.route('/api',methods=['POST'])
-def predict():
-    # Get the data from the POST request.
+@app.route("/", methods=['POST'])
+def hello():
 	data = request.get_json(force=True)
-	predict = model.predict(data['feature'])
-	return jsonify(predict[0].tolist())
+	# if request.method == 'POST':
+	# 	print("POST'd details::", request.json["details"])	
+	res = predict(data['details'])
+	return jsonify(str(res))
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+
--- inference.py
+++ inference.py
@@ -1,9 +1,9 @@
 import torch
 
-def predict():
+def predict(data):
     # Example or inference. This model returns a 1-dim tensor multiplied by 2
     ts = torch.jit.load('./doubleit_model.pt')
-    sample_tensor = torch.tensor([1, 2, 3, 4])
+    sample_tensor = torch.tensor(data)
     result = ts(sample_tensor)
-    print(result)  # <- tensor([2, 4, 6, 8])
+    # print(result)  # <- tensor([2, 4, 6, 8]) 
     return result
