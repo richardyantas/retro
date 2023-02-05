533cd5c83c7142e9558269e696cc10d43c746486
Richard Yantas <richard@dysopsis.com>
2023-02-05 14:13:09 -0500

    updated

--- ci-cd/bootstrap.sh
+++ ci-cd/bootstrap.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
-sudo apt-get update && \ 
-sudo apt-get install -yq build-essential python3-pip rsync git  && \ 
+sudo apt-get update 
+sudo apt-get install -yq build-essential python3-pip rsync git
 
-# install docker
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# install docker https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-22-04
 
-sudo apt-get update && \
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin && \
-
-
-git clone https://github.com/richardyantas/tenpo.git  && \ 
-cd tenpo/ && \ 
-docker build -t mlops:latest . && \ 
+git clone https://github.com/richardyantas/retro.git
+cd retro/backend/
+docker build -t mlops:latest .
 docker run -d -p 5000:5000 mlops 
